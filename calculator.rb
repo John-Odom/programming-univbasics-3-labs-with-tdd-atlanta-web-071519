2b32b8382a0bd41ba37becf08e9166e01f89a9fc
John Odom <joodom91@gmail.com>
2019-06-13 15:28:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,4 @@
 # Add your variables here
 first_number = 10
-second_variable = 20.0
-
-def calculator (num1, operator, num2)
-  
-  return num1 + operator + num2
-end
+second_number = 20.0
+sum = first_number+second_number
